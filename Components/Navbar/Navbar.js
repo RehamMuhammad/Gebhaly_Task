e8a7339bbb6c46d48df03895ec149c4524dbaa49
Reham Muhammad <reham.muhammad.helmy@gmail.com>
2021-06-10 16:57:21 +0200

    run build

--- Components/Navbar/Navbar.js
+++ Components/Navbar/Navbar.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import {AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography} from '@material-ui/core';
+import {AppBar, Toolbar, IconButton, Badge} from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
 import navStyles from '../../styles/Nav.module.scss';
-import Link from 'next/Link';
+import NextLink  from 'next/Link';
 import  Image  from 'next/image';
 
 const Navbar = ({totalCartItems}) => {
     return (
         <>
                 <nav className={navStyles.appBar}>              
-                <Link href="/"  > 
+                <NextLink href="/"  > 
                 <a className={navStyles.logo}><Image src='/gebhaly.png' alt="Gebhaly" height={40} width={130}  /></a>
-                </Link>
+                </NextLink>
                 </nav>
                 <div className={navStyles.grow} />
                 <div  className={navStyles.icon}>
-                    <Link href="/Cart/cart">
+                    <NextLink href="/Cart/cart">
                     <IconButton  aria-label="Show cart items" color="inherit" >
                         <Badge badgeContent={totalCartItems} color="error">
                             <ShoppingCart />
                         </Badge>
                     </IconButton>
-                    </Link>
+                    </NextLink>
                 </div>
         </>
     )
